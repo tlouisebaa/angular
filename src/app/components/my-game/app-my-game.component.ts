5781b62fc7f86ad94cf37a7c059b067a22d90760
tlouisebaa <gadorabegail24@gmail.com>
2022-09-14 09:01:25 +0800

    make it work

--- src/app/components/my-game/app-my-game.component.ts
+++ src/app/components/my-game/app-my-game.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: 'app-my-game',
   template: `<div *ngFor="let datum of data">
-              <button>{{datum}}</button>
+              <button>{{datum.key}}</button>
             </div>`,
 })
 
